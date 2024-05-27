14d4f0ea4e7de8ffc9898393e8a8c5daceb28a60
Richard <ngangarichard335@gmail.com>
2024-05-27 17:01:24 +0300

    done

--- lib/person.py
+++ lib/person.py
@@ -1,7 +1,16 @@
 #!/usr/bin/env python3
 
 class Person:
-    # Class body goes here
+    def talk(self):
+        print("Hello World!")
+    
+    def walk(self):
+        print("The person is walking.")
 
-    #Instance method definition
-    pass
+
+    
+
+Mike= Person()
+print(Mike)
+print(Mike.talk())
+print(Mike.walk())
